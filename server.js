96673be4ff93de7c3558b52ea97c774582ca73b8
kalivaraprasad gonapa <77320853+Streen9@users.noreply.github.com>
2023-06-18 05:20:53 +0530

    server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
