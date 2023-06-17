ad33c09a9ad0fd161e67657f567d029c48355ca5
kalivaraprasad gonapa <77320853+Streen9@users.noreply.github.com>
2023-06-18 04:06:15 +0530

    server

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/api/data', (req, res) => {
   // Read the data from the JSON file and send it as a response
   const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
   res.json(data);
+  res.send('api working')
 });
 
 app.post('/api/data', (req, res) => {
@@ -20,10 +21,11 @@ app.post('/api/data', (req, res) => {
   const newData = req.body;
   fs.writeFileSync('data.json', JSON.stringify(newData));
   res.json({ message: 'Data saved successfully' });
+  res.send('This is my about route..... ')
 });
 
 // Start the server
 const port = 3000;
-app.listen(port,ipAddress, () => {
+app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
